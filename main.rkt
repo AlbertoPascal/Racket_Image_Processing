e7eece670e2f30c661d8ae07c70507caec557925
AlbertoPascal <beto_pascal@hotmail.com>
2019-11-30 18:02:22 -0600

    Added functions to prepare for message encryption

--- main.rkt
+++ main.rkt
@@ -6,29 +6,81 @@
             [mat_size (read-matrix-size in)] ; we store x and y coordinates from our matrix size
             [pixels (read-all-pixels in (* (car mat_size) (car (cdr mat_size))) '())]
         )
-        (write img_type)
-        (display "\n")
-        (write mat_size)
-        (display "\n")
-        (write pixels)
-        (close-input-port in)
-        pixels
+        ;(write img_type)
+        ;(display "\n")
+        ;(write mat_size)
+        ;(display "\n")
+        ;(write pixels)
+        ;(display "\n")
+        (close-input-port in) 
+        (append (list img_type) (list mat_size) (list(convert-pixels-to-bin pixels '()) ) )
     ;Idea: leer los parámetros de la ppm image para saber cuantos renglones y columnas hay. A partir de
     ;ahí, hacer tercias y guardar esas listas de tercias dentro de otra lista para representar la lista de pixeles.
     ;(append (read-line in)
     )
 )
-(define (testlet var) ; this is just to test the value a let function returns. 
+(define (encode-msg msg img-name)
     (let*
-       (
-        [x 1]
-        [y 3]
-        [z (+ x y)]
+        (
+            [init_msg_length (string-length msg)]
+            [img_data (read-image img-name)]
+            [msg_arr (prep-list (string-append (~v init_msg_length) " " msg) '())]
+            [img_type (car img_data)]
+            [mat_size (car (cdr img_data))]
+            [pixel_arr (car (cdr (cdr img_data)))]
         )
-        (+ x y)
+        (write msg_arr)
+        (display "\n")
+        (write img_type)
+        (display "\n")
+        (write mat_size)
+        (display "\n")
+        (write pixel_arr)
+        (display "\n")
+
+        (if (valid-encryption msg_arr pixel_arr)
+            (write "I can encrypt this message")
+            (write "message is too long for this image. Please try with a larger image")
+        )
+
+
     )
-    
 )
+(define (encrypt-message msg_arr pixel_arr new_pixel_arr)
+    (if (> (length msg_arr) 0)
+        ;send next letter to encryption. 
+        1
+        1
+    )
+)
+(define (get_trimmed_pixel pixel)
+    (if (null? (cdr pixel))
+        '()
+        (append (list (car pixel)) (get_trimmed_pixel (cdr pixel)))
+    )
+)
+(define (add_next_bit char_bit trimmed_pixel)
+    (string-append trimmed_pixel char_bit)
+)
+()
+; (add_next_bit "1" (Join-chars (get_trimmed_pixel (string->list "100"))))
+
+(define (valid-encryption msg_arr pixel_arr)
+    (let*
+        (
+            [msg_length (* (length msg_arr) 8)]
+            [chnl_length (length pixel_arr)]
+            ;message includes length when written. 
+
+        )
+        
+        (if (> msg_length chnl_length)
+            #f
+            #t
+        )
+    )
+)
+
 (define (read-image-type input) ;This method returns the image type. For example, P6
     (read input)
 )
@@ -49,10 +101,8 @@
             [read-G (read input)]
             [read-B (read input)]
         )
-        (if (eof-object? read-R)
-            pixel_list
-            (list (append (list read-R) (list read-G) (list read-B)))
-        )
+        (list (append (list read-R) (list read-G) (list read-B)))
+        
     )
 )
 (define (char-bin character)
@@ -63,9 +113,10 @@
 (define (prep-list sentence binary-arr)
 ;This function will receive a word or sentence, convert it into a list of characters and send char by char to its binary conversion.
 ;Function returns an array of strings containing the binary representation of each letter  
+    ;(write sentence)
     (if (> (string-length sentence) 0)
         ;If I still have letters, I will return the letter's binary component.
-        (prep-list (Join-chars (cdr (string->list sentence))) (append binary-arr (list (char-bin (car (string->list sentence))))))
+        (prep-list (Join-chars (cdr (string->list sentence))) (append binary-arr (list (format-bin-string (char-bin (car (string->list sentence)))))))
         ;If I am out of letters, I need to return my array of "bits"
         binary-arr
         
@@ -94,9 +145,15 @@
     )
 
 )
+(define (format-bin-string binstring)
+    (if (< (string-length binstring) 8)
+        (format-bin-string (string-append "0" binstring))
+        binstring
+    )
+)
 (define (convert-pixels-to-bin pixels new_pixels)
     (if (> (length pixels) 0)
-        (convert-pixels-to-bin (cdr pixels) (append new_pixels (list (convert-list-to-bin (car pixels) '()))))
+        (convert-pixels-to-bin (cdr pixels) (append new_pixels (convert-list-to-bin (car pixels) '())))
         ;(convert-pixles-to-bin (cdr pixles) (append new_pixels (convert-list-to-bin (car pixels) '())))
         new_pixels
     )
@@ -105,7 +162,7 @@
 (define (convert-list-to-bin elem_list new_list)
     (if (> (length elem_list) 0)
         ;(cdr elem_list)
-        (convert-list-to-bin (cdr elem_list) (append new_list (list (DecToBin (car elem_list)))))
+        (convert-list-to-bin (cdr elem_list) (append new_list (list (format-bin-string (DecToBin (car elem_list))))))
         new_list
     )
 )
