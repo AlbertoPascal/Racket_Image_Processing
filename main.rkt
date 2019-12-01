c131d55a8027595150540afe0f7a0d3b428734cc
AlbertoPascal <beto_pascal@hotmail.com>
2019-12-01 16:11:48 -0600

    Added threading cappabilities for massive image creation. Needs refining for a single image splitting

--- main.rkt
+++ main.rkt
@@ -1,5 +1,42 @@
 ;NOTE: MAIN FUNCTIONS ARE (encode-msg msg input-ppm output-ppm) and (decode-msg "input-ppm" "")
 
+
+;===============TESTING OF THREADS =============================
+
+; Create a new semaphore
+(define semaphore-out (make-semaphore 1))
+
+; Function to create new threads
+(define (make-thread name)
+    (thread (lambda ()
+             (let loop
+                ([n 0])
+                (if (< n 10)
+                    ; true
+                    (begin
+                        (sleep (random))
+                        (semaphore-wait semaphore-out)
+                        ; Critical section
+                        (write name)
+                        (display "\n")
+                        (encode-msg "h" "input.txt" (string-append name "output" (number->string n) ".ppm") )
+                        (display "\n")
+                        ;(printf "Thread: ~a | Counter: ~a\n" name n)
+                        ;(printf "~a_~a " name n)
+                        (semaphore-post semaphore-out)
+                        (loop (+ n 1)))
+                    ; false
+                    (printf "Thread ~a finishing\n" name))))))
+
+; Main function to test
+(define (main)
+    (printf "MAIN THREAD START\n")
+    (define threads (map make-thread '("Thread_A" "Thread_B" "Thread_C")))
+    ; Apply a function to each element in a list, without results
+    (for-each thread-wait threads)
+    (printf "MAIN THREAD FINISHING\n"))
+
+;=================ENDS TESTING OF THREADS==========================
 (define (read-image img-name)
     (let*
         (
